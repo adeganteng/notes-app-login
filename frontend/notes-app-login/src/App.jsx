847606ebfa92ebd086226bfcbe5755e7064685af
adeganteng <wicaksonoade21@gmail.com>
2024-06-16 16:21:29 +0700

    3. react-router-dom installation

--- frontend/notes-app-login/src/App.jsx
+++ frontend/notes-app-login/src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/SignUp/Signup";
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path="/dashboard" exact element={<Home />} />
+      <Route path="/login" exact  element={<Login />} />
+      <Route path="/signup" exact element={<Signup />} />
+    </Routes>
+  </Router>
+)
 
 function App() {
   return (
     <>
-      <Home />
+      {routes}
     </>
   );
 }
