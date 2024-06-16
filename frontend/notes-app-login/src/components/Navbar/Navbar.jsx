ae70eda6e3251b1e5393565e4692a9b85825cc25
adeganteng <wicaksonoade21@gmail.com>
2024-06-16 17:29:47 +0700

    4. Login Ui Screen & Password Input component

---
+++ frontend/notes-app-login/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
+      <h2 className="text-xl font-medium text-black py-2 transition-all hover:underline ">
+        Notes <span className="text-primary">App</span>
+      </h2>
+    </div>
+  );
+};
+
+export default Navbar;
--- frontend/notes-app-login/src/pages/Login/Login.jsx
+++ frontend/notes-app-login/src/pages/Login/Login.jsx
@@ -1,7 +1,66 @@
+import { useState } from "react";
+import PasswordInput from "../../components/Input/PasswordInput";
+import Navbar from "../../components/Navbar/Navbar";
+import { Link } from "react-router-dom";
+import { validateEmail } from "../../utils/helper";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+ const [error, setError] = useState(null);
+  const handleLogin = async(e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address");
+      return
+    }
+
+    if (!password) {
+      setError("Please enter your password");
+      return
+    }
+
+    setError("")
+
+    // Login API Call
+  }
   return (
     <div>
-      <h1>Login</h1>
+      <Navbar />
+
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+          <form onSubmit={handleLogin}>
+            <h4 className="text-2xl mb-7">Login</h4>
+
+            <input
+              type="text"
+              placeholder="Email"
+              className="input-box"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+
+            <button type="submit" className="btn-primary">
+              Login
+            </button>
+
+            <p className="text-center mt-3 text-sm">
+              Not registerd yet?{" "}
+              <Link to={"/signup"} className="text-primary underline">
+                Create an account
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
